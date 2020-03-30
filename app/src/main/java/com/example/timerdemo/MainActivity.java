f138c9d6a329e68cb6cb6fee129a2fbc5786354d
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-30 19:32:46 +0200

    Timer erste Variante

--- app/src/main/java/com/example/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/timerdemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.timerdemo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Hey wir sinds", "Eine Sekunde ist vergangen");
+
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.post(run);
+
     }
 }
